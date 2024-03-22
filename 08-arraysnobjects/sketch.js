21a85ace8778a852f6363c1451d12f471bf3fdfd
greffen <161754813+greffen@users.noreply.github.com>
2024-03-22 14:08:28 -0600

    sound
    added sounds to the arrays and object notation project

--- 08-arraysnobjects/sketch.js
+++ 08-arraysnobjects/sketch.js
@@ -3,8 +3,17 @@
 // Date
 //
 // Extra for Experts:
-// 
+// Implements audio (and by extension the p5 audio library), 
 
+let clickSounds = [];
+let totalClickSounds = 7;
+
+function preload() {
+  //adds the sounds to the clickSounds array
+  for (let i = 0; i < totalClickSounds; i++) {
+    clickSounds[i] = loadSound("Audio Files/click" + i + ".mp3");
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,3 +22,16 @@ function setup() {
 function draw() {
   background(220);
 }
+
+function mouseClicked() {
+  //code for actually playing the sounds
+  let randomInt;
+  randomInt = int(random(0, totalClickSounds));
+  clickSounds[randomInt].play();
+}
+
+//add particles (this will hopefully be the object notation)
+//add gameplay
+//add cookie
+//add upgrades
+//add acheivments (or even save files?)
