2871f5e346864cfc8de915201eeaf970f56368ee
greffen <161754813+greffen@users.noreply.github.com>
2024-03-27 14:11:45 -0600

    added like so much
    moved images into a folder
    added the title
    added the cookie bounce
    added the cookie counter
    finished all the collision and sound stuff
    probably more that i forgot about

--- 08-arraysnobjects/sketch.js
+++ 08-arraysnobjects/sketch.js
@@ -3,38 +3,38 @@
 // 3/29/24
 //
 // Extra for Experts:
-// Implements audio (and by extension the p5 audio library), 
+// Implements audio (and by extension the p5 audio library), uses the lerp function in default p5js,  
 
-let cookie = [];
+let cookies = 0;
 let clickSounds = [];
 let totalClickSounds = 7;
 let cookieIMG;
-let scalar = 1.0;
+let cookieSize = 360;
+let currentClickValue = 1;
+let radius = 180;
+let currentRadius = 120;
+
 
 function preload() {
   //adds the sounds to the clickSounds array
   for (let i = 0; i < totalClickSounds; i++) {
     clickSounds[i] = loadSound("Audio Files/click" + i + ".mp3");
   }
+  //loads the cookie image
+  cookieIMG = loadImage("Image Files/cookie.png");
 
-  cookieIMG = loadImage("cookie.png");
 }
 
 function setup() {
+  imageMode(CENTER);
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  //ellipse(width/18 + 180, height/4 + 170, 360);
-  
-  drawCookie();
-}
-
-function drawCookie() {
-  //cookieIMG.mouseOver(scalar = 1.5);
-  //cookieIMG.mouseOut(scalar = 1);
-  image(cookieIMG, width/18, height/4, cookieIMG.width * scalar, cookieIMG.height * scalar);
+  //ellipse(width/18 + 180, height/4 + 170, cookieSize); //fake cookie hitbox thing
+  drawCookieCount();
+  thatBounceEffect();
 }
 
 function playCookieClick() {
@@ -44,30 +44,48 @@ function playCookieClick() {
   clickSounds[randomNum].play();
 }
 
-// function mousePressed() {
-//   //did you click on the cookie check
-//   for (let thing of cookie) {
-//     let clickedInCookie = pointCircle(mouseX, mouseY, width/18 + 180, height/4 + 170);
-//     if (clickedInCookie) {
-//       scalar = 1.5;
-//       playCookieClick();
-//     }
-//     else {
-//       //do nothing
-//     }
-//   }
-// } i dont know why this doesnt work
+function mousePressed() {
+  //did you click on the cookie check
+  let clickedInCookie = pointCircle(mouseX, mouseY, width/18 + 180, height/4 + 170);
+  if (clickedInCookie) {
+    playCookieClick();
+    currentRadius = 80;
+    cookies = cookies + currentClickValue;
+  }
+  else {
+    //do nothing
+  }
+}
 
 function pointCircle(px, py, cx, cy) {
+  //the small amount of "are you clicking on the cookie" math
   let d = dist(px, py, cx, cy);
-   
-  if(d <= size/2) {
+  if(d <= cookieSize/2) {
     return true; 
   }
   return false;
 }
 
+function drawCookieCount() {
+  //text formatting
+  noStroke();
+  fill(255);
+  textSize(80);
+  textAlign(CENTER, CENTER);
+  //displaying number of clicks
+  text(cookies, width/2, height/2);
+}
+
+function thatBounceEffect() {
+  //draws the cookie and makes it "bounce" on click
+  currentRadius = lerp(currentRadius, radius, 0.3);
+  image(cookieIMG, width/18 + 180, height/4 + 170, currentRadius * 2, currentRadius * 2);
+}
+
+
+//do something with the title that you added
 //add particles (this will hopefully be the object notation)
 //add gameplay
 //add upgrades
+//add autoclicker
 //add acheivments (or even save files?)
