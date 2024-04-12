47fcf081a846d9209f86ea809c6b33f21445b315
greffen <161754813+greffen@users.noreply.github.com>
2024-04-12 14:11:41 -0600

    conway!

---
+++ 11-gameoflife/sketch.js
@@ -0,0 +1,194 @@
+// 2D Grid
+// Dan Schellenberg
+// Apr 11, 2024
+
+// if you are hard-coding a level, I'd use something like this
+
+// let grid = [[1, 0, 0, 1],
+//             [0, 1, 0, 1],
+//             [1, 1, 0, 0],
+//             [1, 0, 1, 1],
+//             [0, 0, 0, 1],
+//             [0, 0, 1, 1],
+//             [0, 1, 0, 1],
+//             [0, 0, 0, 1]];
+
+let grid;
+let cellSize;
+let switcher = true;
+const GRID_SIZE = 30;
+let isAutoPlayOn = false;
+
+function setup() {
+  //make the canvas the largest square possible
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+
+  //if randomizing the grid, do this:
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  
+  //this is dumb -- should check if this is the right size!
+  cellSize = height/grid.length;
+}
+
+function windowResized() {
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+
+  cellSize = height/grid.length;
+}
+
+function draw() {
+  background(220);
+
+  if (isAutoPlayOn && frameCount % 5 === 0) {
+    grid = updateGrid();
+  }
+
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+  if (key === "s") {
+    switcher =!switcher ;
+  }
+
+  if (key === " ") {
+    grid = updateGrid();
+  }
+
+  if (key === "a") {
+    isAutoPlayOn = !isAutoPlayOn;
+  }
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+  //always toggle self
+  toggleCell(x, y);
+
+  if (switcher === true) {
+    //toggle self + NSEW tiles if s is pressed
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
+  }
+
+}
+
+function toggleCell(x, y) {
+//make sure that the cell youre toggleing is in the grid
+  if (x < GRID_SIZE && y < GRID_SIZE &&
+    x >= 0 && y >= 0) { 
+    //toggle the color of the cell
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else {
+      grid[y][x] = 0;
+    }
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      //half the time, be a 1. Other half, be a 0.
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function updateGrid() {
+  //need a second array to not mess with the grid while counting the neighbours
+  let nextTurn = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+
+  //look at every cell
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      let neighbours = 0;
+
+      //look at every cell in a 3x3 grid around the cell chosen above
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //avoid going off grid
+          if (y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE) {
+            neighbours += grid[y+i][x+j];
+          }
+        }
+      }
+      //dont count yourself bumbo
+      neighbours -= grid[y][x];
+
+      //apply the "Conway's Game of Lifetm" rules
+      if (grid[y][x] === 1) { //currently alive
+        if (neighbours === 2 || neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0) { //currently dead
+        if (neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+    }
+  }
+  return nextTurn;
+}
