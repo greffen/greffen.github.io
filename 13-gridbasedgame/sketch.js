3c47f387e88a764402d0d6c5597ce1f1612e966a
greffen <161754813+greffen@users.noreply.github.com>
2024-04-24 14:11:11 -0600

    way better pellet approach

--- 13-gridbasedgame/sketch.js
+++ 13-gridbasedgame/sketch.js
@@ -3,39 +3,45 @@
 // Apr 15, 2024
 //
 // Extra for Experts:
-// Implements audio
+// Implements .inside and .flat (.flat took so much searching for and is a godsend. actually so beautiful), implements audio (which i guess is no longer actually extra for experts), implements 
 
 let grid = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,],
-  [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,],
-  [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,],
-  [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1,],
-  [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,],
-  [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1,],
-  [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [1, 1, 1, 1, 1, 0, 1, 0, 1, 10, 11, 12, 1, 0, 1, 0, 1, 1, 1, 1, 1,],
-  [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1,],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,],
-  [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,],
-  [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1,],
-  [1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1,],
-  [1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1,],
-  [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,],
-  [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1,],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1,],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1,],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,],
+  [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1,],
+  [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1,],
+  [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1,],
+  [1, 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1, 1,],
+  [2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 10, 11, 12, 1, 2, 1, 2, 1, 1, 1, 1, 1,],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1,],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1,],
+  [1, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 1,],
+  [1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 1, 1, 1,],
+  [1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1,],
+  [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1,],
+  [1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1,],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],];
 let cellSize;
 const GRID_SIZE = 21;
 const PLAYER = 9;
+const PLAYER2 = 8;
+const PELLET_TILE = 2;
 const BLOCK = 1;
 const OPEN_TILE = 0;
 const BLINKY = 10;
 const PINKY = 11;
 const CLYDE = 12;
 let player = {
-  x: 10,
+  x: 9,
+  y: 14,
+};
+let player2 = {
+  x: 11,
   y: 14,
 };
 let titleIMG;
@@ -63,9 +69,6 @@ function setup() {
   else {
     createCanvas(windowHeight, windowHeight);
   }
-
-  //if randomizing the grid, do this:
-  //grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
   
   //this is dumb -- should check if this is the right size!
   cellSize = height/grid.length;
@@ -75,14 +78,6 @@ function setup() {
 
   //equalize audio
   loadJingleMusic.setVolume(0.4);
-
-  // store pellets in an array
-  for (let y=0; y< GRID_SIZE; y++) {
-    for (let x=0; x< GRID_SIZE; x++) {
-      let thePellet = new Pellet(x, y);
-      pelletArray.push(thePellet);
-    }
-  }
 }
 
 function windowResized() {
@@ -107,19 +102,34 @@ function draw() {
   else if (state === "game") {
     displayGrid();
     updateMovement();
-    displayPellets();
+    if (grid.flat(2).includes(2) === false) {
+      state = "win";
+    }
+  }
+  else if (state === "win") {
+    background(255, 253, 201);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    textFont("Crackman");
+    text("<WE'VE GOT A WINNER>", width/2, height/2);
   }
   else if (state === "death screen") {
     background(255, 253, 201);
+    textSize(80);
+    textAlign(CENTER, CENTER);
+    textFont("Crackman");
+    text("you loose", width/2, height/4);
+    text("WOMP WOMP", width/2, height/1.25);
+
 
     // Define the positions for each sad image
     let centerX = canvas.width / 2;
     let centerY = canvas.height / 2;
-    let padding = 20; // Adjust this value to control the spacing between images
-    let offsetX = 100; // Adjust this value to move the images to the right
-    let offsetY = 0; // Adjust this value to move the images down
+    let padding = 20; 
+    let offsetX = 100;
+    let offsetY = 0;
 
-    // Draw each sad image at a static position on the screen
+    //draw each sad image at a static position on the screen(in the most overengineered way possible)
     let totalWidth = 0;
     let maxImageHeight = 0;
     let sadImages = [sad1, sad2, sad3, sad4];
@@ -206,7 +216,7 @@ function movePlayerAccordingToDirection() {
   }
 
   // only if the next position is open
-  if (grid[nextY][nextX] === OPEN_TILE) {
+  if (grid[nextY][nextX] === OPEN_TILE || grid[nextY][nextX] === PELLET_TILE) {
     //clear old player
     grid[player.y][player.x] = OPEN_TILE;
     //update new player
@@ -242,6 +252,7 @@ function movePlayerAccordingToDirection() {
   }
 }
 
+
 function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
@@ -272,12 +283,22 @@ function displayGrid() {
         square(x * cellSize, y * cellSize, cellSize);
       }
       else if (grid[y][x] === OPEN_TILE) {
-        //image(pavementIMG, x * cellSize, y * cellSize, cellSize);
         fill("black");
         square(x * cellSize, y * cellSize, cellSize);
       }
+      else if (grid[y][x] === PELLET_TILE) {
+        let radius = (width/GRID_SIZE+height/GRID_SIZE)/2*0.1;
+        fill("black");
+        square(x * cellSize, y * cellSize, cellSize);
+        fill("white");
+        circle(x*width/GRID_SIZE + width/GRID_SIZE/2, y*height/GRID_SIZE + height/GRID_SIZE/2, radius*2);
+      }
       else if(grid[y][x] === PLAYER) {
         fill ("yellow");
+        square(x * cellSize, y * cellSize, cellSize);
+      }
+      else if(grid[y][x] === PLAYER2) {
+        fill ("green");
         square(x * cellSize, y * cellSize, cellSize);
       }
       else if(grid[y][x] === BLINKY) {
@@ -307,38 +328,9 @@ function generateEmptyGrid(cols, rows) {
   return emptyArray;
 }
 
-//most all pellet stuff is found below
-class Pellet {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.radius = (width/GRID_SIZE+height/GRID_SIZE)/2*0.1;
-    this.theColor = "white";
-  }
-
-  display() {
-    if (grid[this.y][this.x] === 0) {
-      fill(this.theColor);
-      circle(this.x*width/GRID_SIZE + width/GRID_SIZE/2, this.y*height/GRID_SIZE + height/GRID_SIZE/2, this.radius*2);
-    }
-  }
-}
-
-function displayPellets() {
-  let p = 0; // position within the pellet array
-  for (let y=0; y < GRID_SIZE; y++) {
-    for (let x=0; x < GRID_SIZE; x++) {
-      if (grid[y][x] === 0) { 
-        pelletArray[p].display();
-      }
-      p++;
-    }
-  }
-}
-
 
 //to do:
 //better end screen
-//balls
 //animation loop
 //ghosts ai
+//lerp movement
